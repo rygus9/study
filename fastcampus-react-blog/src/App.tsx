1cd8759d36da548b575cf54e92624b5b529106f0
Cuzz <rygus9@naver.com>
2023-11-04 16:06:20 +0900

    feat: 초기 세팅 라우팅 설정

--- fastcampus-react-blog/src/App.tsx
+++ fastcampus-react-blog/src/App.tsx
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
+import { Link, Navigate, Route, Routes } from "react-router-dom"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/posts">Posts</Link>
+        </li>
+        <li>
+          <Link to="/posts/:id">Posts Detail</Link>
+        </li>
+        <li>
+          <Link to="/posts/new">Posts New</Link>
+        </li>
+        <li>
+          <Link to="/posts/edit/:id">Posts Update</Link>
+        </li>
+        <li>
+          <Link to="/profile">Profile</Link>
+        </li>
+      </ul>
+      <Routes>
+        <Route path="/" element={<h1>메인</h1>} />
+        <Route path="/posts" element={<h1>포스트</h1>} />
+        <Route path="/posts/:id" element={<h1>포스트 디테일</h1>} />
+        <Route path="/posts/new" element={<h1>포스트 생성</h1>} />
+        <Route path="/posts/edit/:id" element={<h1>포스트 수정</h1>} />
+        <Route path="/profile" element={<h1>프로필</h1>} />
+        <Route path="*" element={<Navigate replace to="/"></Navigate>}></Route>
+      </Routes>
+    </>
+  )
 }
 
-export default App;
+export default App
